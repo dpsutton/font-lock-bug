09e3803cbfff54328bd29defeb8e035bb153cbb1
Dan Sutton <danielsutton01@gmail.com>
2017-05-20 16:07:00 -0500

    adds CIDER var description
    describe this var to see what the font-locking infrastructure is doing

--- src/font_lock_bug/core.clj
+++ src/font_lock_bug/core.clj
@@ -8,4 +8,11 @@
 
 (defn uses-import
   [x]
-  (i/bar x))
+  (i/bar 2))
+
+;; describe-var:
+;; cider--dynamic-font-lock-keywords is a variable defined in ‘cider-mode.el’.
+;; Its value is
+;; (("\\_<\\(foo\\|i/bar\\|uses-import\\)\\_>" 0
+;;   (cider--unless-local-match font-lock-function-name-face)))
+;; Local in buffer font_lock_bug/core.clj; global value is nil
